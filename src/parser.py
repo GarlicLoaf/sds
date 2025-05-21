b2e83d60c418408ee9e68639fecceb50870f6a64
GarlicLoaf <garic15@protonmail.com>
2025-05-21 07:55:25 +0200

    First working version

--- src/parser.py
+++ src/parser.py
@@ -2,39 +2,58 @@ import json
 from pathlib import Path
 
 INPUT_PATH = Path("./input/")
+OUTPUT_PATH = Path("./output/")
 
 
-def read_lines(content):
+def read_lines(content, read_idx):
     json = {}
-    for i, line in enumerate(content):
-        line = line.strip()
-        match line[0]:
-            case "-":
-                # standard output
-                json["line_" + str(i)] = {"type": 0, "text": line[2:]}
-            case "!":
-                # branch
-                if not json:
-                    json["line_" + str(i)] = {
-                        "type": 1,
-                        "text": line[2:],
-                        "content": read_lines(content[i:]),
-                    }
-                else:
-                    return json
-            case ">":
-                # decision
-                json["line_" + str(i)] = {"type": 2, "text": line[2:]}
-    return json
+    last_exclamation = None
+    choice = 0
+    line_counter = 0
+
+    while read_idx < len(content):
+        line = content[read_idx].strip()
+        if line.startswith("-"):
+            # standard output
+            json["line_" + str(line_counter)] = {"type": 0, "text": line[2:]}
+            read_idx += 1
+            line_counter += 1
+        elif line.startswith("!"):
+            # branch
+            choice = 0
+            last_exclamation = line_counter
+            json["line_" + str(line_counter)] = {
+                "type": 1,
+                "text": line[2:],
+                "answers": [],
+                "branches": {},
+            }
+            read_idx += 1
+            line_counter += 1
+        elif line.startswith(">"):
+            # decision
+            json["line_" + str(last_exclamation)]["answers"].append(line[2:])
+            branch_json, new_read_idx = read_lines(content, read_idx + 1)
+            json["line_" + str(last_exclamation)]["branches"][str(choice)] = branch_json
+            choice += 1
+            read_idx = new_read_idx
+        else:
+            return json, read_idx + 1
+    return json, read_idx
 
 
 def main():
     for file_path in INPUT_PATH.glob("*.sds"):
-        json_dict = {}
+        json_dict = None
         with open(file_path, "r") as file:
             contents = file.readlines()
-        json_dict = read_lines(contents)
+        json_dict, _ = read_lines(contents, 0)
         print(json_dict)
+
+        output_file = OUTPUT_PATH / (file_path.stem + ".json")
+
+        with open(output_file, "w", encoding="utf-8") as out_file:
+            json.dump(json_dict, out_file, indent=4, ensure_ascii=False)
 
 
 if __name__ == "__main__":
